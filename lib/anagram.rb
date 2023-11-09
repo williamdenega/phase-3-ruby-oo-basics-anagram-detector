7f3e93768f306d09393c09e0f38f5ff074a5c34d
William Denega <williamdenega@gmail.com>
2023-11-08 21:02:38 -0500

    commit tree a1af35f69b83d4f4aa1cf30abba3c45228974c54

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,41 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+    attr_reader :word
+
+    def initialize word
+        @word = word
+    end
+
+    def match list
+        list.select {|str| anagram?(str)  } 
+    end
+
+    private
+
+    def anagram? str
+        word.chars.sort == str.chars.sort 
+    end
+
+end
+
+class Anagram_v2
+
+    attr_reader :arry, :word
+
+    def initialize word
+        @word = word
+        @arry = word.chars.sort
+    end
+
+    def match list
+        list.select {|str| word.chars.sort == str.chars.sort  } 
+    end
+
+end
+ 
+ 
+#test = Anagram.new "test"
+
+#binding.pry
